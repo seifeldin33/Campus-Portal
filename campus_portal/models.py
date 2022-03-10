456957b34b449803e9699ebf0188801d23334419
NOURELDIN OSAMA <58091991+NourEldin-Osama@users.noreply.github.com>
2022-03-10 14:23:29 +0200

    Phase 1: Change salary from Decimal to Float

--- campus_portal/models.py
+++ campus_portal/models.py
@@ -57,7 +57,7 @@ class Student(Person):
 class Employee(Person):
     employee_ID = models.IntegerField(primary_key=True, unique=True)
     department = models.CharField(max_length=10)
-    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    salary = models.FloatField()
 
     def __str__(self):
         return self.name
