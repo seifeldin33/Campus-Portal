8c2a34a689fad6da96591c5617eb71637d69d5dd
NOURELDIN OSAMA <58091991+NourEldin-Osama@users.noreply.github.com>
2022-03-16 23:08:29 +0200

    Phase 2: 1) Change models.py "change in requirement" 2) add url to our system "http://localhost:8000/campus_portal/" 3) link the basic front-end with back-end

--- campus_portal/models.py
+++ campus_portal/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.core.exceptions import ValidationError
+from django.db import models
 
 
 # Create your models here.
@@ -11,15 +11,6 @@ class Person(models.Model):
     gender = models.CharField(max_length=1, choices=TEMP_CHOICES, default=MALE)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
-    address = models.CharField(max_length=100)
-    national_id = models.IntegerField(max_length=14)
-    name_in_arabic = models.CharField(max_length=100)
-    address_in_arabic = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    citizenship = models.CharField(max_length=20)
-    mother_tongue = models.CharField(max_length=20)
-    birth_place = models.CharField(max_length=100)
-    zip_code = models.CharField(null=True, max_length=5)
     picture = models.ImageField(null=True, upload_to="uploads/students_picture/")
     admitted_in = models.DateField(auto_now_add=True)
 
@@ -27,18 +18,12 @@ class Person(models.Model):
         abstract = True
 
     def save(self, *args, **kwargs):
-        r = int(str(self.national_id)[-2]) % 2
-        if (r == 0) and (self.gender == "M"):
-            raise ValidationError('You Entered Fake National ID')
-        if (r == 1) and (self.gender == "F"):
-            raise ValidationError('You Entered Fake National ID')
         self.full_clean()
         super(Person, self).save(*args, **kwargs)
 
 
 class Student(Person):
     Student_ID = models.IntegerField(primary_key=True, unique=True)
-    program = models.CharField(max_length=10)
     school = models.CharField(max_length=10)
     major = models.CharField(max_length=10)
     concentration = models.CharField(max_length=10)
@@ -53,13 +38,8 @@ class Student(Person):
 
     gpa = models.DecimalField(max_digits=4, decimal_places=2, validators=[validate_gpa])
     percent = models.DecimalField(max_digits=4, decimal_places=2)
-    admission_status = models.CharField(max_length=10)
-    academic_status = models.CharField(max_length=10)
     total_credit_hours = models.IntegerField()
     number_of_subjects = models.IntegerField()
-    thanawya_grade = models.DecimalField(max_digits=4, decimal_places=2)
-    thanawya_percent = models.DecimalField(max_digits=4, decimal_places=2)
-    thanawya_type = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
---
+++ campus_portal/old_models.py
@@ -0,0 +1,99 @@
+# from django.db import models
+# from django.core.exceptions import ValidationError
+#
+#
+# # Create your models here.
+# class Person(models.Model):
+#     name = models.CharField(max_length=100)
+#     birthdate = models.DateField()
+#     MALE, FEMALE = 'M', 'F'
+#     TEMP_CHOICES = ((MALE, 'Male'), (FEMALE, 'Female'))
+#     gender = models.CharField(max_length=1, choices=TEMP_CHOICES, default=MALE)
+#     email = models.EmailField()
+#     phone_number = models.CharField(max_length=15)
+#     address = models.CharField(max_length=100)
+#     national_id = models.IntegerField(max_length=14)
+#     name_in_arabic = models.CharField(max_length=100)
+#     address_in_arabic = models.CharField(max_length=100)
+#     city = models.CharField(max_length=100)
+#     citizenship = models.CharField(max_length=20)
+#     mother_tongue = models.CharField(max_length=20)
+#     birth_place = models.CharField(max_length=100)
+#     zip_code = models.CharField(null=True, max_length=5)
+#     picture = models.ImageField(null=True, upload_to="uploads/students_picture/")
+#     admitted_in = models.DateField(auto_now_add=True)
+#
+#     class Meta:
+#         abstract = True
+#
+#     def save(self, *args, **kwargs):
+#         r = int(str(self.national_id)[-2]) % 2
+#         if (r == 0) and (self.gender == "M"):
+#             raise ValidationError('You Entered Fake National ID')
+#         if (r == 1) and (self.gender == "F"):
+#             raise ValidationError('You Entered Fake National ID')
+#         self.full_clean()
+#         super(Person, self).save(*args, **kwargs)
+#
+#
+# class Student(Person):
+#     Student_ID = models.IntegerField(primary_key=True, unique=True)
+#     program = models.CharField(max_length=10)
+#     school = models.CharField(max_length=10)
+#     major = models.CharField(max_length=10)
+#     concentration = models.CharField(max_length=10)
+#     level = models.IntegerField(default=1)
+#     cohort = models.IntegerField()
+#
+#     def validate_gpa(gpa):
+#         if gpa > 4:
+#             raise ValidationError(r"No one's GPA is greater than 4")
+#         elif gpa < 0:
+#             raise ValidationError(r"No one's GPA is smaller than 0")
+#
+#     gpa = models.DecimalField(max_digits=4, decimal_places=2, validators=[validate_gpa])
+#     percent = models.DecimalField(max_digits=4, decimal_places=2)
+#     admission_status = models.CharField(max_length=10)
+#     academic_status = models.CharField(max_length=10)
+#     total_credit_hours = models.IntegerField()
+#     number_of_subjects = models.IntegerField()
+#     thanawya_grade = models.DecimalField(max_digits=4, decimal_places=2)
+#     thanawya_percent = models.DecimalField(max_digits=4, decimal_places=2)
+#     thanawya_type = models.CharField(max_length=10)
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Meta:
+#     ordering = ["-Student_ID", "name"]
+#
+#
+# class Employee(Person):
+#     employee_ID = models.IntegerField(primary_key=True, unique=True)
+#     department = models.CharField(max_length=10)
+#     salary = models.FloatField()
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Doctor(Employee):
+#     degree = models.CharField(max_length=10)
+#
+#     class Meta:
+#         ordering = ["name"]
+#
+#
+# class Course(models.Model):
+#     code = models.CharField(primary_key=True, max_length=10)
+#     name = models.CharField(max_length=40)
+#     credit_hour = models.IntegerField()
+#     prerequistes = models.CharField(null=True, max_length=10)
+#     type = models.CharField(max_length=10)
+#
+#     def __str__(self):
+#         return self.name
+#
+#     class Meta:
+#         ordering = ["-name"]
