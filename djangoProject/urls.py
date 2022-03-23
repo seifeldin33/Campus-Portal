4e1089217e60e69e25faa5b1a33bd7fb2927d318
NOURELDIN OSAMA <58091991+NourEldin-Osama@users.noreply.github.com>
2022-03-23 20:20:09 +0200

    clean the files

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    # path('',include())
     path('admin/', admin.site.urls),
     path('campus_portal/', include('campus_portal.urls')),
 ]
