018468a8f0899bff43aee37b4bc4dc48ee1aa307
NOURELDIN OSAMA <58091991+NourEldin-Osama@users.noreply.github.com>
2022-03-24 01:42:09 +0200

    finish signup.html

--- campus_portal/urls.py
+++ campus_portal/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.get, name='index'),
     path('', views.index, name='index'),
+    path('Login', views.Login, name='Login'),
+    path('signup', views.signup, name='signup'),
 ]
--- campus_portal/views.py
+++ campus_portal/views.py
@@ -16,21 +16,35 @@ def get(request):
     return render(request, 'index.html', context)
 
 
+def Login(request):
+    context = {'title': 'N'}
+    return render(request, 'Login.html', context)
+
+
 def signup(request):
     if request.method == "POST":
-        pass
-    #     if request.POST['password1'] == request.POST['password2']:
-    #         try:
-    #             User.objects.get(username=request.POST['username'])
-    #             return render(request, 'accounts/signup.html', {'error': 'Username is already taken!'})
-    #         except User.DoesNotExist:
-    #             user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-    #             auth.login(request, user)
-    #             return redirect('home')
-    #     else:
-    #         return render(request, 'accounts/signup.html', {'error': 'Password does not match!'})
-    # else:
-    #     return render(request, 'accounts/signup.html')
+        print(request)
+        print(request.read())
+    context = {'title': 'N'}
+    return render(request, 'signup.html', context)
+
+
+#
+# def signup(request):
+#     if request.method == "POST":
+#         pass
+#     if request.POST['password1'] == request.POST['password2']:
+#         try:
+#             User.objects.get(username=request.POST['username'])
+#             return render(request, 'accounts/signup.html', {'error': 'Username is already taken!'})
+#         except User.DoesNotExist:
+#             user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+#             auth.login(request, user)
+#             return redirect('home')
+#     else:
+#         return render(request, 'accounts/signup.html', {'error': 'Password does not match!'})
+# else:
+#     return render(request, 'accounts/signup.html')
 
 
 def login(request):
